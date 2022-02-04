ab760a731a1ee19feed5568643e6ab4fa9c6f176
Travis Bordovsky <travis@blacklocus.com>
2022-02-04 13:23:37 -0600

    Formatting

--- src/test/java/com/tbordovsky/debian/DebianPluginTest.java
+++ src/test/java/com/tbordovsky/debian/DebianPluginTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DebianPluginTest {
-    @Test public void pluginRegistersATask() {
+
+    @Test
+    public void pluginRegistersATask() {
         // Create a test project and apply the plugin
         Project project = ProjectBuilder.builder().build();
         project.getPlugins().apply("com.tbordovsky.debian");
